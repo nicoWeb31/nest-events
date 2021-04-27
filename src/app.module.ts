09045fd5372a7d34b4ed21f61b3e0070b6dac3dd
NicoRiot <nriot@umoove-inc.com>
2021-04-27 18:23:00 +0200

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Event } from './event.entity';
 import { EventsController } from './events.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: '127.0.0.1',
+    port: 5432,
+    username: 'postgres',
+    password: 'example',
+    database: 'nest-events',
+    entities: [Event],
+    synchronize:true,
+  }),],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
